0e02a60a366804fa1a2c3fc0b6d9a5b1b8c32c27
zJosueDavid <josue_castillo_narvaez@hotmail.com>
2023-10-15 17:30:24 -0500

    Intento de Actividad Contacto InformacionVIewModel

--- 24CV_AppWeb/Controllers/FormulariosController.cs
+++ 24CV_AppWeb/Controllers/FormulariosController.cs
@@ -39,9 +39,57 @@ namespace _24CV_AppWeb.Controllers
             mail.To.Add(email);
             mail.Subject = "Notificación de contacto";
             mail.IsBodyHtml = true;
-            mail.Body= $"Se ha recibido información del correo <h1>{email}</h1> <br/> <p>>{comentario}/<p";
+            mail.Body= $"Se ha recibido información del correo <h1>{email}</h1> <br/> <p>{comentario}</p>";
             smtp.Send(mail);
             return true;
+        }
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
+        public IActionResult EnviarContacto(InformacionViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ProcesarInformacion(model))
+                {
+                    TempData["Mensaje"] = "Comentario enviado con éxito.";
+                }
+                else
+                {
+                    TempData["Mensaje"] = "Error al enviar el comentario.";
+                }
+            }
+            return View("Contacto", model);
+        }
+        public bool ProcesarInformacion(InformacionViewModel model)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient smtp = new SmtpClient("mail.shapp.mx", 587);
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("moises.puc@shapp.mx", "Dhaserck_999");
+                mail.From = new MailAddress("moises.puc@shapp.mx", "Administrador");
+                mail.To.Add(model.Email);
+                mail.Subject = "Notificación de contacto";
+                mail.IsBodyHtml = true;
+                mail.Body = $"<h1>Solicitud de contacto</h1> " +
+                            $"<p>Nombre: {model.Nombre}</p>" +
+                            $"<p>Apellido: {model.Apellido}</p>" +
+                            $"<p>Email: {model.Email}</p>" +
+                            $"<p>Fecha de Nacimiento: {model.FechaNacimiento}</p>" +
+                            $"<p>Turno: {model.Turno}</p>" +
+                            $"<p>Comentarios: {model.Comentarios}</p>";
+                smtp.Send(mail);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Error al enviar el correo", ex);
+            }
         }
     }
 }
---
+++ 24CV_AppWeb/Models/InformacionViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace _24CV_AppWeb.Models
+{
+    public class InformacionViewModel
+    {
+        public string Nombre { get; set; } 
+        public string Apellido {  get; set; }
+        public string Email { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Turno { get; set; }
+
+        public string Comentarios { get; set; }
+
+    }
+}
